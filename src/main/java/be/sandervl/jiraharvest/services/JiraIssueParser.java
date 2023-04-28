fcd1891f3a8f68924f4d261b338e504a83b042b9
lab900-sander-van-loock <sander.vanloock@lab900.com>
2023-04-28 12:12:00 +0200

    take into account multiple days for estimated time in hours

--- src/main/java/be/sandervl/jiraharvest/services/JiraIssueParser.java
+++ src/main/java/be/sandervl/jiraharvest/services/JiraIssueParser.java
@@ -1,11 +1,16 @@
 package be.sandervl.jiraharvest.services;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -32,51 +37,37 @@ public class JiraIssueParser {
   }
 
   public Optional<LocalDate> getWorkedOnTimeForIssue(JiraService.BasicIssue issue) {
-    var statusChange =
-        issue.changelog().histories().stream()
-            .flatMap(
-                history ->
-                    history.items().stream()
-                        .filter(
-                            item ->
-                                item.fieldId() != null
-                                    && item.fromString() != null
-                                    && item.toStringJava() != null
-                                    && item.fieldId().equalsIgnoreCase("status"))
-                        .map(
-                            item ->
-                                new StatusChange(
-                                    item.fromString(),
-                                    item.toStringJava(),
-                                    LocalDateTime.parse(history.created(), formatter))))
-            .toList();
+    List<StatusChange> statusChange = getStatusChanges(issue);
 
     return statusChange.stream()
-        .filter(sc -> sc.time != null && sc.from().equalsIgnoreCase("To Do"))
+        .filter(sc -> sc.time != null && sc.to().equalsIgnoreCase("In Progress"))
         .min(Comparator.comparing(StatusChange::time))
         .map(StatusChange::time)
         .map(LocalDateTime::toLocalDate);
   }
 
+  private List<StatusChange> getStatusChanges(JiraService.BasicIssue issue) {
+    return issue.changelog().histories().stream()
+        .flatMap(
+            history ->
+                history.items().stream()
+                    .filter(
+                        item ->
+                            item.fieldId() != null
+                                && item.fromString() != null
+                                && item.toStringJava() != null
+                                && item.fieldId().equalsIgnoreCase("status"))
+                    .map(
+                        item ->
+                            new StatusChange(
+                                item.fromString(),
+                                item.toStringJava(),
+                                LocalDateTime.parse(history.created(), formatter))))
+        .toList();
+  }
+
   public Optional<Duration> getWorkedTimeForIssue(JiraService.BasicIssue issue) {
-    var statusChange =
-        issue.changelog().histories().stream()
-            .flatMap(
-                history ->
-                    history.items().stream()
-                        .filter(
-                            item ->
-                                item.fieldId() != null
-                                    && item.fromString() != null
-                                    && item.toStringJava() != null
-                                    && item.fieldId().equalsIgnoreCase("status"))
-                        .map(
-                            item ->
-                                new StatusChange(
-                                    item.fromString(),
-                                    item.toStringJava(),
-                                    LocalDateTime.parse(history.created(), formatter))))
-            .toList();
+    List<StatusChange> statusChange = getStatusChanges(issue);
 
     Optional<LocalDateTime> startingTime =
         statusChange.stream()
@@ -88,7 +79,36 @@ public class JiraIssueParser {
             .filter(sc -> sc.time != null && sc.from().equalsIgnoreCase("In Progress"))
             .max(Comparator.comparing(StatusChange::time))
             .map(StatusChange::time);
-    return startingTime.flatMap(st -> endingTime.map(et -> Duration.between(st, et)));
+
+    return startingTime.flatMap(
+        start ->
+            endingTime.map(
+                end -> {
+                  if (start.toLocalDate().equals(end.toLocalDate())) {
+                    return Duration.between(start, end);
+                  }
+
+                  LocalTime startWorkingTime = LocalTime.of(9, 0);
+                  LocalTime endWorkingTime = LocalTime.of(17, 0);
+
+                  return Stream.iterate(start, date -> date.plusDays(1))
+                      .limit(ChronoUnit.DAYS.between(start, end.plusDays(1)) + 1)
+                      .filter(
+                          date ->
+                              date.getDayOfWeek() != DayOfWeek.SATURDAY
+                                  && date.getDayOfWeek() != DayOfWeek.SUNDAY)
+                      .map(
+                          date -> {
+                            LocalDateTime startOfWorkingTime = date.with(startWorkingTime);
+                            LocalDateTime endOfWorkingTime = date.with(endWorkingTime);
+                            LocalDateTime startOfOverlap =
+                                start.isBefore(startOfWorkingTime) ? startOfWorkingTime : start;
+                            LocalDateTime endOfOverlap =
+                                end.isAfter(endOfWorkingTime) ? endOfWorkingTime : end;
+                            return Duration.between(startOfOverlap, endOfOverlap);
+                          })
+                      .reduce(Duration.ZERO, Duration::plus);
+                }));
   }
 
   public record StatusChange(String from, String to, LocalDateTime time) {}
